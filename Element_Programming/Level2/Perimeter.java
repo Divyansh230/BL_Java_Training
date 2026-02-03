faf1046fe890c0917653cc5df9bd9a6ca775fbcc
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 14:19:17 +0530

    Add. Area of triangle is being added

---
+++ Element_Programming/Level2/Perimeter.java
@@ -0,0 +1,12 @@
+package Element_Programming.Level2;
+
+import java.util.Scanner;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int perimeter=sc.nextInt();
+        System.out.println("Side of the Square: "+(perimeter/4));
+    }
+    
+}
