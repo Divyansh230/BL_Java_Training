81499c76008c68e9f7b5c1efa3a99915b536f02a
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 11:45:20 +0530

    Add. Marks. java has been created

---
+++ Element_Programming/Level1/Marks.java
@@ -0,0 +1,7 @@
+public class Marks {
+    public static void main(String[] args) {
+        int maths=94,physics=95,chemistry=96;
+        int total=maths+physics+chemistry;
+        System.out.println("Average Marks:" + (total/3));
+    }
+}
