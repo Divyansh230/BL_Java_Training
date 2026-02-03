52b3ef85673a69029bee0fe3572614ac7e60361f
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 11:42:14 +0530

    Add. Age Calculator Program

---
+++ Element_Programming/Level1/CurrentAge.java
@@ -0,0 +1,8 @@
+class CurrentAge{
+    public static void main(String[] args){
+        int birthYear=2000,currentYear=2024;
+        int age=currentYear-birthYear;
+        System.out.println("Current Age: "+age);
+        
+    }
+}
