16e28ba27920f34df94eea0e9cee7cea215bf9fa
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 11:51:41 +0530

    Profit Loss is being added

---
+++ Element_Programming/Level1/Profit_Loss.java
@@ -0,0 +1,10 @@
+public class Profit_Loss {
+    public static void main(String[] args) {
+        double cp=129;
+        double sp=191;
+
+        System.out.println("Profit:"+(sp-cp));
+        System.out.println("Profit Percentage:" + (((sp - cp) / cp) * 100));
+    }
+    
+}
