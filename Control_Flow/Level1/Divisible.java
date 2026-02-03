168eeb29793c6a1a79b17dd575fa0cc490112bd2
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 15:47:59 +0530

    Add . Number divisble by 5 is checked

---
+++ Control_Flow/Level1/Divisible.java
@@ -0,0 +1,17 @@
+package Control_Flow.Level1;
+
+import java.util.Scanner;
+
+public class Divisible {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num%5==0){
+            System.out.println("Divisible by 5");
+        }
+        else{
+            System.out.println("Not Divisible by 5");
+        }
+    }
+    
+}
