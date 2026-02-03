81185c03ea8288b4629a6898b1807af2f024ce3b
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 14:17:40 +0530

    add. Created Area of Triangle code

---
+++ Element_Programming/Level2/Area_Of_Triangle.java
@@ -0,0 +1,14 @@
+package Element_Programming.Level2;
+
+import java.util.Scanner;
+
+public class Area_Of_Triangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double base=sc.nextDouble();
+        double height=sc.nextDouble();
+        double area=0.5*base*height;
+        System.out.println("Area of Triangle: %.2f"+area);
+    }
+    
+}
