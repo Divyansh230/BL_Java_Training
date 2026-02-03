89c58240145c560abf4d48e31d7d76c46711ac32
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 11:48:14 +0530

    add . Km_To_is added to the repo

---
+++ Element_Programming/Level1/Km_To_Miles.java
@@ -0,0 +1,7 @@
+public class Km_To_Miles {
+    public static void main(String[] args) {
+        double km = 10;
+        double miles = km * 0.621371;
+        System.out.println(km + " km = " + miles + " miles");
+    }
+}
