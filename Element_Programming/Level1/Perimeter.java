cd427d94dc38abee9ee2e0f82238c5d69bbaced7
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 12:24:20 +0530

    Perimeter of the square is being calculated

---
+++ Element_Programming/Level1/Perimeter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int perimeter=sc.nextInt();
+
+        System.out.println("Side of Square: "+(perimeter/4));
+    }
+    
+}
