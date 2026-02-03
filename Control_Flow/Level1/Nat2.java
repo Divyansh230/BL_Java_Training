a681bc72a6919132bd3d7ed960297b8cae6ff2a3
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-03 16:07:44 +0530

    Add. Sum is being initialized

---
+++ Control_Flow/Level1/Nat2.java
@@ -0,0 +1,17 @@
+package Control_Flow.Level1;
+
+import java.util.Scanner;
+
+public class Nat2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        int sum2=n*(n+1)/2;
+        System.out.println("Comparison: "+(sum==sum2));
+    }
+    
+}
