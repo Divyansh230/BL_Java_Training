6d60330e462bd198209ae053cdcf1b4a1d0d7f2a
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-04 10:34:03 +0530

    Add. Prime Number code is being added to git

---
+++ Control_Flow/Level3/Prime.java
@@ -0,0 +1,22 @@
+package Control_Flow.Level3;
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num=scanner.nextInt();
+        boolean isPrime=true;
+        if(num<=1){
+            isPrime=false;
+        } else {
+            for(int i=2;i<=Math.sqrt(num);i++){
+                if(num%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+        }
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+    
+}
