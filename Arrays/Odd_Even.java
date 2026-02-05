bb7b6fd425ca20cd4f66ce95a72e09a150ece93a
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-05 14:26:31 +0530

    Add. Odd and even numbers is being calculated

---
+++ Arrays/Odd_Even.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.*;
+public class Odd_Even {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int odd[]=new int[n];
+        int even[]=new int[n];
+        for(int i=0;i<n;i++){
+            int x=sc.nextInt();
+            if(x%2==0){
+                even[i]=x;
+            }
+            else{
+                odd[i]=x;
+            }
+        }
+        System.out.println("Even numbers: "+Arrays.toString(even));
+        System.out.println("Odd numbers: "+Arrays.toString(odd));
+    }
+    
+}
