2d508e09b7dd7f078f7cdc424fb33d0f0ed0b9e7
Divyansh230 <divyanshsinghh2304@gmail.com>
2026-02-05 16:04:06 +0530

    Add . Frequency of the Element has been calculated

--- Arrays/Level2/Frequency.java
+++ Arrays/Level2/Frequency.java
@@ -1,9 +1,25 @@
 package Arrays.Level2;
-
+import java.util.Map;
+import java.util.Scanner;
+import java.util.HashMap;
 public class Frequency {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int 
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+      Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println("Element\tFrequency");
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
     }
     
 }
