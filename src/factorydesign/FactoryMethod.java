867978e6a2e3a0e18f935c975ffde375f5733226
Divyansh Singh <divyanshsinghh2304@gmail.com>
2026-02-22 21:48:13 +0530

    Add . Factory design is being completed

---
+++ src/factorydesign/FactoryMethod.java
@@ -0,0 +1,93 @@
+package factorydesign;
+
+
+// Product Interface and subclasses
+interface BBurger {
+    void prepare();
+}
+
+class BasicBBurger implements Burger {
+    public void prepare() {
+        System.out.println("Preparing Basic Burger with bun, patty, and ketchup!");
+    }
+}
+
+class StandardBBurger implements Burger {
+    public void prepare() {
+        System.out.println("Preparing Standard Burger with bun, patty, cheese, and lettuce!");
+    }
+}
+
+class PremiumBBurger implements Burger {
+    public void prepare() {
+        System.out.println("Preparing Premium Burger with gourmet bun, premium patty, cheese, lettuce, and secret sauce!");
+    }
+}
+
+class BasicWheatBBurger implements Burger {
+    public void prepare() {
+        System.out.println("Preparing Basic Wheat Burger with bun, patty, and ketchup!");
+    }
+}
+
+class StandardWheatBBurger implements Burger {
+    public void prepare() {
+        System.out.println("Preparing Standard Wheat Burger with bun, patty, cheese, and lettuce!");
+    }
+}
+
+class PremiumWheatBBurger implements Burger {
+    public void prepare() {
+        System.out.println("Preparing Premium Wheat Burger with gourmet bun, premium patty, cheese, lettuce, and secret sauce!");
+    }
+}
+
+// Factory Interface and Concrete Factories
+interface BBurgerFactory {
+    Burger createBurger(String type);
+}
+
+class SinghBBurger implements BBurgerFactory {
+    public Burger createBurger(String type) {
+        if (type.equalsIgnoreCase("basic")) {
+            return new BasicBurger();
+        } else if (type.equalsIgnoreCase("standard")) {
+            return new StandardBurger();
+        } else if (type.equalsIgnoreCase("premium")) {
+            return new PremiumBurger();
+        } else {
+            System.out.println("Invalid burger type!");
+            return null;
+        }
+    }
+}
+
+class KingBBurger implements BBurgerFactory {
+    public Burger createBurger(String type) {
+        if (type.equalsIgnoreCase("basic")) {
+            return new BasicWheatBurger();
+        } else if (type.equalsIgnoreCase("standard")) {
+            return new StandardWheatBurger();
+        } else if (type.equalsIgnoreCase("premium")) {
+            return new PremiumWheatBurger();
+        } else {
+            System.out.println("Invalid burger type!");
+            return null;
+        }
+    }
+}
+
+// Main Class
+public class FactoryMethod {
+    public static void main(String[] args) {
+        String type = "basic";
+
+        BBurgerFactory myFactory = new SinghBBurger();
+        Burger burger = myFactory.createBurger(type);
+
+        if (burger != null) {
+            burger.prepare();
+        }
+    }
+}
+
